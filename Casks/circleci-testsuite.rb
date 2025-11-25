cc6051a1aea341b0344ff06191ecadb7545fc3f5
Gordon Syme <gordonsyme@gmail.com>
2025-11-25 16:21:02 +0000

    TEST-72 Bump testsuite plugin to 1.0.14861-46d09ab
    This is the first signed + notarized binary

--- Casks/circleci-testsuite.rb
+++ Casks/circleci-testsuite.rb
@@ -1,9 +1,9 @@
 cask "circleci-testsuite" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.0.14861-46d09ab"
-  sha256 arm: "6b5925f30209d32776dfdf8954444fb3d9895bd11c0d1bf227009dc8511214eb",
-         intel: "235f82a0cff3227ac3467e02fc6d74dae8279274a553c36ff5ec2f9ddc2c808a"
+  version "1.0.14935-630104a"
+  sha256 arm: "119efb069d5c3d9f0c2fd40b674ebf9a812dd8c2dbdf9fe17bf0514d52d10906",
+         intel: "897f48bef23709d18834540f0a04ad23c081e84872d6fbdaa1066cdddc1b841e"
 
   url "https://circleci-binary-releases.s3.amazonaws.com/circleci-cli-plugins/circleci-testsuite/#{version}/darwin/#{arch}/circleci-testsuite.gz"
   name "CircleCI TestSuite Plugin"
